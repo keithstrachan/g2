881118caac51ace69f01dc9decdd79c24152880a
keithstrachan <keith@strachanit.com.au>
2020-12-30 23:24:54 +1100

    update for ramps fd

--- g2core/boards.mk
+++ g2core/boards.mk
@@ -278,5 +278,24 @@ ifeq ("$(CONFIG)","fourcable")
     SETTINGS_FILE="settings_fourcable.h"
 endif
 
+##########
+# PrintNC configs:
+
+ifeq ("$(CONFIG)","PrintNC-Mill-v1")
+    ifeq ("$(BOARD)","NONE")
+        BOARD=RAMPSFDv1a
+    endif
+    SETTINGS_FILE="settings_printnc-millv1.h"
+endif
+
+ifeq ("$(CONFIG)","PrintNC-Laser-v1")
+    ifeq ("$(BOARD)","NONE")
+        BOARD=RAMPSFDv1a
+    endif
+    SETTINGS_FILE="settings_printnc-laserv1.h"
+endif
+
+
+
 include $(wildcard ./board/$(STAR).mk)
 
